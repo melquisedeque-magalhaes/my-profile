ccda9e959d199b030d5e47862144f2bf12722920
Melqui <melqui.sodre15@gmail.com>
2024-02-01 13:55:51 -0300

    feat: add analytics and metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Heebo, Kalam } from 'next/font/google'
 import { Toaster } from 'sonner'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 const heebo = Heebo({
@@ -31,6 +33,8 @@ export default function RootLayout({
       <body className="bg-surface-background font-heebo text-white scroll-smooth antialiased">
         {children}
         <Toaster />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
